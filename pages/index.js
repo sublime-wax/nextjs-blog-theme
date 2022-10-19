29e375e68a44a61ed37762f0db239608d6514e5d
sublime-wax <116157983+sublime-wax@users.noreply.github.com>
2022-10-19 23:19:35 +0200

    Update index.js
    Revert changes

--- pages/index.js
+++ pages/index.js
@@ -1,48 +1,67 @@
-import Head from 'next/head'
-import Header from '@components/Header'
-import Footer from '@components/Footer'
+import Link from 'next/link';
+import { getPosts } from '../utils/mdx-utils';
 
-export default function Home() {
+import Footer from '../components/Footer';
+import Header from '../components/Header';
+import Layout, { GradientBackground } from '../components/Layout';
+import ArrowIcon from '../components/ArrowIcon';
+import { getGlobalData } from '../utils/global-data';
+import SEO from '../components/SEO';
+
+export default function Index({ posts, globalData }) {
   return (
-    <div className="container">
-      <Head>
-        <title>SublimeWax</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main>
-        <Header title="Boutique de tenue africaine en ligne" />
-        <p className="description">
-          SublimeWax vous propose des tenues africaines: Robe africaine, jupe africaine, des bombers,</br>
-          des hauts africians, des pantalons africians dans des tissus wax 100% en coton.
-        </p>
-        <p className="description">
-          Chaque pièce est unique et confectionné à la main dans nos differents ateliers.Nous choissisions des tissus wax de grande qualité et aux motifs tendances
-        </p>
-         <p className="description">
-          Decouvrez sans plus tarder dans notre boutique:
-         </p>
-       
-        <p className="description">nos collections de <a href="https://www.sublime-wax.com/collections/robes" target="_blank">robes africaines et robes en wax</a> pour toutes occassions</p>
-        <p className="description">de magnifiques <a href="https://www.sublime-wax.com/collections/woman-african-skirts">jupes africaines et jupe en wax</a> pour vous sublimer</p>
-        <p className="description">
-          Notre collection de  <a href="https://www.sublime-wax.com/collections/accessories">bijoux africain</a>, des produits originaux comme nos <a href="https://www.sublime-wax.com/collections/accessories/products/sublimewax-leather-bag-and-bogolan-gana">sacs en cuir, bogolan ou en wax </a>
-        <p className="description">
-          Fait mains par des artisans Kenya <a href="https://www.sublime-wax.com/collections/massai-pearl-earrings">Des bijoux en perles Massai</a>
-        </p>
-        <p className="description">
-          Nous proposons egalement  <a href="https://www.sublime-wax.com/collections/tenue-africaine-pour-couple-sublimewax">des tenues africaines pour couples</a> pour plus de comlicités pour vos ceremonies.
-        </p>
-    
-        <a href="https://https://twitter.com/sublimewax" 
-          class="twitter-follow-button" 
-          data-show-count="false">Follow @sublimewax
-        </a>
-          <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
-    
+    <Layout>
+      <SEO title={globalData.name} description={globalData.blogTitle} />
+      <Header name={globalData.name} />
+      <main className="w-full">
+        <h1 className="text-3xl lg:text-5xl text-center mb-12">
+          {globalData.blogTitle}
+        </h1>
+        <ul className="w-full">
+          {posts.map((post) => (
+            <li
+              key={post.filePath}
+              className="md:first:rounded-t-lg md:last:rounded-b-lg backdrop-blur-lg bg-white dark:bg-black dark:bg-opacity-30 bg-opacity-10 hover:bg-opacity-20 dark:hover:bg-opacity-50 transition border border-gray-800 dark:border-white border-opacity-10 dark:border-opacity-10 border-b-0 last:border-b hover:border-b hovered-sibling:border-t-0"
+            >
+              <Link
+                as={`/posts/${post.filePath.replace(/\.mdx?$/, '')}`}
+                href={`/posts/[slug]`}
+              >
+                <a className="py-6 lg:py-10 px-6 lg:px-16 block focus:outline-none focus:ring-4">
+                  {post.data.date && (
+                    <p className="uppercase mb-3 font-bold opacity-60">
+                      {post.data.date}
+                    </p>
+                  )}
+                  <h2 className="text-2xl md:text-3xl">{post.data.title}</h2>
+                  {post.data.description && (
+                    <p className="mt-3 text-lg opacity-60">
+                      {post.data.description}
+                    </p>
+                  )}
+                  <ArrowIcon className="mt-4" />
+                </a>
+              </Link>
+            </li>
+          ))}
+        </ul>
       </main>
+      <Footer copyrightText={globalData.footerText} />
+      <GradientBackground
+        variant="large"
+        className="fixed top-20 opacity-40 dark:opacity-60"
+      />
+      <GradientBackground
+        variant="small"
+        className="absolute bottom-0 opacity-20 dark:opacity-10"
+      />
+    </Layout>
+  );
+}
 
-      <Footer />
-    </div>
-  )
+export function getStaticProps() {
+  const posts = getPosts();
+  const globalData = getGlobalData();
+
+  return { props: { posts, globalData } };
 }
